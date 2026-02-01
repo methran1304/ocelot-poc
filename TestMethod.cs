faaf4c1616617c28203928a1831536d2bd098725
Methran <methran2003@gmail.com>
2026-02-01 18:15:25 +0000

    dummy test utils

---
+++ TestMethod.cs
@@ -0,0 +1,8 @@
+namespace ocelot_poc
+{
+    public static class OcelotMisc
+    {
+        public static int AddNumber(int a, int b) => a + b;
+        public static int MulNumber(int a, int b) => a * b;
+    }
+}
